6cbd580f223f372250ba754182be06b85e65406a
Thane-Fanfulik <31455026+Thane-Fanfulik@users.noreply.github.com>
2018-02-20 17:57:26 -0600

    Added www. to textbox

--- Web Crawler 2.0/WebCrawler/UI.Designer.cs
+++ Web Crawler 2.0/WebCrawler/UI.Designer.cs
@@ -77,7 +77,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(260, 20);
             this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "http://";
+            this.textBox1.Text = "http://www.";
             // 
             // UI
             // 
